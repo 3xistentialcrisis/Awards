ef665b77a7f9cbd4ea3a8ca1a6b6e94f57200958
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-12 00:49:56 +0300

    Create profile view

--- ourawards/views.py
+++ ourawards/views.py
@@ -10,16 +10,20 @@ def index(request):
 #User Signup
 def signup(request):
     if request.method == 'POST':
-    form = SignupForm(request.POST)
-    if form.is_valid():
+        form = SignupForm(request.POST)
+        if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('index')
-    else:
-        form = SignupForm()
+        else:
+            form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form })
+
+#Profile
+def profile(request, username):
+    return render(request, 'profile.html')
 
 
