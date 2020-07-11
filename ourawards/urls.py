975a129fdfcef5bf57899ebaf31e5a1dc09dde4a
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-12 00:43:41 +0300

    Import static and settings in app urls

--- ourawards/urls.py
+++ ourawards/urls.py
@@ -1,5 +1,7 @@
-from django.urls import url, include
+from django.conf.urls import url, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'ourawards'
 
@@ -8,3 +10,6 @@ urlpatterns = [
     url(r'signup/', views.signup, name='signup'),
     url(r'account/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
