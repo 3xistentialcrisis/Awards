ea177783fed0f9b4c5848bef2878b3e8f2d47cb2
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-11 23:55:27 +0300

    Create User signup view

--- ourawards/views.py
+++ ourawards/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .forms import SignupForm
 
 # Create your views here.
+#Index Page
 def index(request):
     return render(request, 'index.html')
 
-
+#User Signup
 def signup(request):
-    return render(request, 'registration/signup.html')
+    form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form })
+
+
