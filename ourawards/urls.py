55fbcd0a674d8e4150034731346820fa0fbf31ce
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-12 14:13:18 +0300

    import routers and create api url patterns

--- ourawards/urls.py
+++ ourawards/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('profile', views.ProfileViewSet)
 
 app_name = 'ourawards'
 
@@ -9,6 +13,8 @@ urlpatterns = [
     url('^$', views.index, name='index'),
     url(r'signup/', views.signup, name='signup'),
     url(r'account/', include('django.contrib.auth.urls')),
+    url(r'api/', include(router.urls)),
+    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'profile/(?P<username>.+)/$', views.profile, name='profile'),
 ]
 
