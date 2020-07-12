19712699209d860266306772a1cb49a549a14bf6
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-12 16:08:48 +0300

    Create Edit Profile View

--- ourawards/views.py
+++ ourawards/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm, PostForm
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm
 from rest_framework import viewsets
 from .models import Profile,  Post
 from .serializers import ProfileSerializer, UserSerializer
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 #Index Page
@@ -52,8 +54,29 @@ def signup(request):
     return render(request, 'registration/signup.html', {'form': form })
 
 #Profile
+@login_required(login_url='/accounts/login/')
 def profile(request, username):
+
     return render(request, 'profile.html')
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form
+    }
+    return render(request, 'edit.html', params)
 
 #Project
 def project(request, post):
