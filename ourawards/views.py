227017d494be9de804a41177d1a798407bd5359c
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-12 16:20:44 +0300

    Add ratingsform() to project view

--- ourawards/views.py
+++ ourawards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm
+from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm, RatingsForm
 from rest_framework import viewsets
 from .models import Profile,  Post
 from .serializers import ProfileSerializer, UserSerializer
@@ -81,8 +81,10 @@ def edit_profile(request, username):
 #Project
 def project(request, post):
     post = Post.objects.get(title=post)
-
+    [design, usability, content] = [[0], [0], [0]]
+    form = RatingsForm()
     params = {
-        'post': post
+        'post': post,
+        'rating_form': form
     }
     return render(request, 'project.html', params)
