82abd48d9319886a795d4869dc4919fd6131e4ea
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-13 12:34:07 +0300

    Deploy to heroku

--- ourawards/urls.py
+++ ourawards/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
