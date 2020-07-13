cc6d574500d41cd153eb31747f2e05a50b87a1bc
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-13 14:28:20 +0300

    Deploy to heroku

--- ourawards/urls.py
+++ ourawards/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^profile/(?P<username>\w+)', views.profile, name='profile'),
     url(r'(?P<username>/profile)', views.user_profile, name='userprofile'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^profile/(?P<username>)', views.edit_profile, name='edit'),
+    url(r'^profile/(?P<username>/settings)', views.edit_profile, name='edit'),
     url(r'^project/(?P<post>)', views.project, name='project'),
     url(r'search/', views.search_project, name='search'),
 ]
